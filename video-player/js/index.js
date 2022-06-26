a356bed8b3287170c35c6c8b3cd5769ef76d08b3
Taras Savchenko <hiddenroadss@gmail.com>
2022-06-27 02:10:54 +0300

    added new icons

--- video-player/js/index.js
+++ video-player/js/index.js
@@ -1,30 +1,82 @@
-const videoContainer = document.querySelector('.video-player');
-const video = document.querySelector('.video-player__video');
-const playBtn = document.querySelector('.controls__play-pause');
+const videoContainer = document.querySelector(".video-player");
+const video = document.querySelector(".video-player__video");
+const playBtn = document.querySelector(".controls__play-pause");
+const theaterBtn = document.querySelector(".controls__theater");
+const fullScreenBtn = document.querySelector(".controls__full-screen");
+const miniPlayerBtn = document.querySelector(".controls__mini-player");
 
 function togglePlay() {
     video.paused ? video.play() : video.pause();
 }
 
-document.addEventListener('keydown', (e) => {
-    switch(e.key.toLowerCase()) {
-        case 'k':
-        case ' ':
+function toggleTheaterMode() {
+    videoContainer.classList.toggle("theater");
+}
+
+function toggleFullScreenMode() {
+    if (document.fullscreenElement == null) {
+        videoContainer.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+}
+
+function toggleMiniPlayerMode() {
+    if (videoContainer.classList.contains("mini-player")) {
+        document.exitPictureInPicture();
+    } else {
+        video.requestPictureInPicture();
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    const tagName = document.activeElement.tagName.toLowerCase();
+    if (tagName === "input") return;
+
+    switch (e.key.toLowerCase()) {
+        case " ":
+            if (tagName === "button") return;
+        case "k":
             togglePlay();
             break;
+        case "f":
+            toggleFullScreenMode();
+            break;
+        case "t":
+            toggleTheaterMode();
+            break;
+        case "i":
+            toggleMiniPlayerMode();
+            break;
     }
-})
+});
 
-playBtn.addEventListener('click', togglePlay)
+document.addEventListener("fullscreenchange", () => {
+    videoContainer.classList.toggle("full-screen", document.fullscreenElement);
+});
 
-video.addEventListener('play', () => {
-    videoContainer.classList.remove('paused');
-})
+document.addEventListener("enterpictureinpicture", () => {
+    videoContainer.classList.add("mini-player");
+});
 
-video.addEventListener('pause', () => {
-    videoContainer.classList.add('paused');
-})
+document.addEventListener("leavepictureinpicture", () => {
+    videoContainer.classList.remove("mini-player");
+});
 
-video.addEventListener('click', () => {
+theaterBtn.addEventListener("click", toggleTheaterMode);
+fullScreenBtn.addEventListener("click", toggleFullScreenMode);
+miniPlayerBtn.addEventListener("click", toggleMiniPlayerMode);
+
+playBtn.addEventListener("click", togglePlay);
+
+video.addEventListener("play", () => {
+    videoContainer.classList.remove("paused");
+});
+
+video.addEventListener("pause", () => {
+    videoContainer.classList.add("paused");
+});
+
+video.addEventListener("click", () => {
     togglePlay();
-})
+});
